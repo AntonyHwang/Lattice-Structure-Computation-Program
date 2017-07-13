c54db54b0fc147593526118203d4d9316ed8af19
Antony hwang <AntonyHwang@hotmail.com>
2017-07-13 13:39:13 +0800

    direction delta calculation added

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def read_nodes(nodes):
             else:
                 words = line.split()
                 if is_number(words[0]):
-                    nodes.append(Node(words[1], words[2], words[3]))
+                    nodes.append(Node(float(words[1]), float(words[2]), float(words[3])))
     node_file.close()
+    return nodes
 
 def read_elements(elements):
     with open('elements.txt') as element_file:
@@ -34,12 +35,28 @@ def read_elements(elements):
                 if is_number(words[0]):
                     elements.append(Element(words[6], words[7], words[8]))
     element_file.close()
+    return elements
+
+def direction_delta(nodes):
+    maxX = max(node.x for node in nodes)
+    minX = min(node.x for node in nodes)
+    maxY = max(node.y for node in nodes)
+    minY = min(node.y for node in nodes)
+    maxZ = max(node.z for node in nodes)
+    minZ = min(node.z for node in nodes)
+    return Node(maxX - minX, maxY - minY, maxZ - minZ)
+
+def construct_lattice(nodes, elements, x, y, z):
+    
 
 def main():
     nodes = []
     elements = []
-    read_nodes(nodes)
-    read_elements(elements)
+    nodes = read_nodes(nodes)
+    elements = read_elements(elements)
+    displacement_factor = direction_delta(nodes)
+    print(str(displacement_factor.x) + " " + str(displacement_factor.y) + " " + str(displacement_factor.z))
+    construct_lattice(nodes, elements, 10, 10, 10)
 
 if __name__ == "__main__":
     main()
