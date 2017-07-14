1c4eca4badef77e0d708d8613d9e6d7d102a9753
Antony hwang <AntonyHwang@hotmail.com>
2017-07-14 09:57:28 +0800

    elements output function added

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+class Lattice(object):
+    def __init__(self, elements, nodes):
+        self.elements, self.nodes = elements, nodes
+
 class Element(object):
     def __init__(self, n1, n2, n3):
         self.n1, self.n2, self.n3 = n1, n2, n3
@@ -33,7 +37,7 @@ def read_elements(elements):
             else:
                 words = line.split()
                 if is_number(words[0]):
-                    elements.append(Element(words[6], words[7], words[8]))
+                    elements.append(Element(int(words[6]), int(words[7]), int(words[8])))
     element_file.close()
     return elements
 
@@ -46,44 +50,65 @@ def direction_delta(nodes):
     minZ = min(node.z for node in nodes)
     return Node(maxX - minX, maxY - minY, maxZ - minZ)
 
-def construct_lattice(shape, nodes, elements, displacement_factor, x, y, z):
+def construct_lattice(shape, nodes, elements, total_nodes, displacement_factor, x, y, z):
+    count = 0;
+    x_nodes = []
+    xy_nodes = []
+    xyz_nodes = []
+
+    x_elements = []
+    xy_elements = []
+    xyz_elements = []
+
     if shape == 90:
-        x_nodes = []
-        xy_nodes = []
-        xyz_nodes = []
         for num in range(0, x):
             for node in nodes:
                 x_nodes.append(Node(node.x + displacement_factor.x, node.y, node.z))
+            for element in elements:
+                x_elements.append(Element(element.n1 + total_nodes, element.n2 + total_nodes, element.n3 + total_nodes))
         for num in range(0, y):
             for node in x_nodes:
                 xy_nodes.append(Node(node.x, node.y + displacement_factor.y, node.z))
+            for element in x_elements:
+                xy_elements.append(Element(element.n1 + total_nodes, element.n2 + total_nodes, element.n3 + total_nodes))
         for num in range(0, z):
             for node in xy_nodes:
                 xyz_nodes.append(Node(node.x, node.y, node.z + displacement_factor.z))
-        return xyz_nodes
+            for element in xy_elements:
+                xyz_elements.append(
+                    Element(element.n1 + total_nodes, element.n2 + total_nodes, element.n3 + total_nodes))
+        return Lattice(set(xyz_elements), set(xyz_nodes))
 
-def write_to_file(nodes, elements):
+def write_to_file(lattice):
     new_node_file = open("output/nodes.txt", "w")
-    new_node_file.write('\n')
-    new_node_file.write(" LIST ALL SELECTED NODES.   DSYS=      0\n")
-    new_node_file.write(" SORT TABLE ON  NODE  NODE  NODE\n")
-    new_node_file.write('\n')
+    #new_node_file.write('\n')
+    #new_node_file.write(" LIST ALL SELECTED NODES.   DSYS=      0\n")
+    #new_node_file.write(" SORT TABLE ON  NODE  NODE  NODE\n")
+    #new_node_file.write('\n')
     count = 0
-    for node in nodes:
+    for node in lattice.nodes:
         count = count + 1
         new_node_file.write("\t" + str(count) + "\t" + str(node.x) + "\t" + str(node.y) + "\t" + str(node.z) + '\n')
     new_node_file.close()
+
+    new_element_file = open("output/elements.txt", "w")
+    count = 0
+    for element in lattice.elements:
+        count = count + 1
+        new_element_file.write("\t" + str(count) + "\t" + str(element.n1) + "\t" + str(element.n2) + "\t" + str(element.n3) + '\n')
+    new_element_file.close()
 
 def main():
     nodes = []
     elements = []
     nodes = read_nodes(nodes)
+    total_nodes = len(nodes)
+    print("nodes per unit: {}".format(total_nodes))
     elements = read_elements(elements)
     displacement_factor = direction_delta(nodes)
     print(str(displacement_factor.x) + " " + str(displacement_factor.y) + " " + str(displacement_factor.z))
-    nodes = construct_lattice(90, nodes, elements, displacement_factor, 10, 10, 10)
-    #calculate elements
-    write_to_file(nodes, elements)
+    lattice = construct_lattice(90, nodes, elements, total_nodes, displacement_factor, 10, 10, 10)
+    write_to_file(lattice)
 
 if __name__ == "__main__":
     main()
