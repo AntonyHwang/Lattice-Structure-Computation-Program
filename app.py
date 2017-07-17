bd4afe8ecc8dd9d53452bf9410e3b29ae97082da
Antony hwang <AntonyHwang@hotmail.com>
2017-07-17 16:47:47 +0800

    need to fix

--- app.py
+++ app.py
@@ -30,10 +30,24 @@ class Element(object):
 
 class Node(object):
     def __init__(self, x, y, z):
-        self.x, self.y, self.z = float(x), float(y), float(z)
+        self.x, self.y, self.z = x, y, z
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
     def toString(self):
-    	return "(" + str(self.x) + "," + str(self.y) + "," + str(self.z) + ")"
-#######################################################################################################################
+        return "(" + str(self.x) + "," + str(self.y) + "," + str(self.z) + ")"
+
+def remove_duplicates(values):
+    output = []
+    seen = set()
+    for value in values:
+        # If value has not been encountered yet,
+        # ... add it to both list and set.
+        if value not in seen:
+            output.append(value)
+            seen.add(value)
+    return output
 
 def is_number(s):
     try:
@@ -81,92 +95,67 @@ def check_node_existence(list, n):
     else:
         return 0
 
+def get_xyz(list, idx):
+    try:
+        return list[idx]
+    except:
+        return idx
+
+
+def get_idx(list, node):
+    return list.index(node)
+
 def construct_lattice(shape, nodes, elements, total_nodes, displacement_factor, x, y, z):
+
+    unique_nodes = list(set(nodes))
     duplicates = 0
-    new_index = 0;
-    node_index = 0
+    # new_index = 0;
+    # node_index = 0
+    # new_nodes = []
     x_nodes = []
     xy_nodes = []
     xyz_nodes = []
 
-    x_elements = []
-    xy_elements = []
-    xyz_elements = []
+    # x_elements = []
+    # xy_elements = []
+    # xyz_elements = []
 
     if shape == 90:
-        for num in range(0, x):
+        for num1 in range(0, x):
             for node in nodes:
-                if check_node_existence(nodes, Node(node.x + num * displacement_factor.x, node.y, node.z)):
-                    #x_elements.append(Element(element.n1 + num * total_nodes - duplicates, element.n2 + num * total_nodes - duplicates, element.n3 + num * total_nodes - duplicates))
-                    x_nodes.append(Node(node.x + num * displacement_factor.x, node.y, node.z))
-                    count = count + 1
-                else:
-                    duplicates = duplicates + 1
-                    # for element in elements:
-                    #     if element.n1 == node_index:
-                    #         element.n1 = new_index
-                    #     if element.n2 == node_index:
-                    #         element.n2 = new_index
-                    #     if element.n3 == node_index:
-                    #         element.n3 = new_index
-            # node_index = node_index + 1
-            for element in elements:
-                x_elements.append(
-                    Element(element.n1 + num * total_nodes - duplicates,
-                            element.n2 + num * total_nodes - duplicates,
-                            element.n3 + num * total_nodes - duplicates))
+                x_nodes.append(Node(node.x + num1 * displacement_factor.x, node.y, node.z))
 
-        for num in range(0, y):
+        for num2 in range(0, y):
             for node in x_nodes:
-                for node in nodes:
-                    if check_node_existence(nodes, Node(node.x + num * displacement_factor.x, node.y, node.z)):
-                        # x_elements.append(Element(element.n1 + num * total_nodes - duplicates, element.n2 + num * total_nodes - duplicates, element.n3 + num * total_nodes - duplicates))
-                        xy_nodes.append(Node(node.x + num * displacement_factor.x, node.y, node.z))
-                        count = count + 1
-                    else:
-                        duplicates = duplicates + 1
-                        # for element in elements:
-                        #     if element.n1 == node_index:
-                        #         element.n1 = new_index
-                        #     if element.n2 == node_index:
-                        #         element.n2 = new_index
-                        #     if element.n3 == node_index:
-                        #         element.n3 = new_index
-                # node_index = node_index + 1
-                for element in elements:
-                    xy_elements.append(
-                        Element(element.n1 + num * total_nodes - duplicates,
-                                element.n2 + num * total_nodes - duplicates,
-                                element.n3 + num * total_nodes - duplicates))
-        for num in range(0, z):
-            for node in nodes:
-                if check_node_existence(nodes, Node(node.x + num * displacement_factor.x, node.y, node.z)):
-                    #x_elements.append(Element(element.n1 + num * total_nodes - duplicates, element.n2 + num * total_nodes - duplicates, element.n3 + num * total_nodes - duplicates))
-                    xyz_nodes.append(Node(node.x + num * displacement_factor.x, node.y, node.z))
-                    count = count + 1
-                else:
-                    duplicates = duplicates + 1
-                    # for element in elements:
-                    #     if element.n1 == node_index:
-                    #         element.n1 = new_index
-                    #     if element.n2 == node_index:
-                    #         element.n2 = new_index
-                    #     if element.n3 == node_index:
-                    #         element.n3 = new_index
-            # node_index = node_index + 1
-            for element in elements:
-                xyz_elements.append(
-                    Element(element.n1 + num * total_nodes - duplicates, element.n2 + num * total_nodes - duplicates, element.n3 + num * total_nodes - duplicates))
+                xy_nodes.append(Node(node.x, node.y + num2 * displacement_factor.y, node.z))
 
-        #removes duplicates
-        # new_nodes = list(set(xyz_nodes))
-        # new_elements = list(set(xyz_elements))
-        print("duplicates idenetified: " + str(duplicates))
+        for num3 in range(0, z):
+            for node in xy_nodes:
+                xyz_nodes.append(Node(node.x, node.y, node.z + num3 * displacement_factor.z))
+
+        for node in xyz_nodes:
+            if node not in unique_nodes:
+                unique_nodes.append(node)
+            else:
+                duplicates += 1
+
+        print("uniq nodes: " + str(len(unique_nodes)))
+
+        # for element_num in range(0, x * y * z - 1):
+        #     elements.append(Element(0, 0, 0))
+        #     n1_idx = get_idx(unique_nodes, get_xyz(xyz_nodes, elements[element_num].n1))
+        #     n2_idx = get_idx(unique_nodes, get_xyz(xyz_nodes, elements[element_num].n2))
+        #     n3_idx = get_idx(unique_nodes, get_xyz(xyz_nodes, elements[element_num].n3))
+        #     elements[element_num].n1 = n1_idx
+        #     elements[element_num].n2 = n2_idx
+        #     elements[element_num].n3 = n3_idx
+
+        print("duplicates identified: " + str(duplicates))
         # #uncomment to test repeats
         #return Lattice(list(set(xyz_elements)), list(set(xyz_nodes)))
 
         # uncomment to test elements
-        return Lattice(xyz_elements, xyz_nodes)
+        return Lattice(elements, unique_nodes)
 
 def write_to_file(lattice):
     new_node_file = open("output/nodes.txt", "w")
@@ -176,10 +165,10 @@ def write_to_file(lattice):
         new_node_file.write("\t" + str(count) + "\t" + str(node.x) + "\t" + str(node.y) + "\t" + str(node.z) + '\n')
     new_node_file.close()
 
-    new_element_file = open("output/elements.txt", "w")
-    count = 0
-    for element in lattice.elements:
-        count = count + 1
+    # new_element_file = open("output/elements.txt", "w")
+    # count = 0
+    # for element in lattice.elements:
+    #     count = count + 1
     #     new_element_file.write("\t" + str(count) + "\t" + lattice.nodes[element.n1].toString() + "\t" + lattice.nodes[element.n2].toString() + "\t" + lattice.nodes[element.n3].toString() + '\n')
     # new_element_file.close()
 
